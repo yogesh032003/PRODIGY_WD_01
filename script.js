5c22dc126e9ebd62ff5f9b03a26634528f91adcc
yogesh032003 <117972724+yogesh032003@users.noreply.github.com>
2024-03-09 10:38:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function imgSlider(anything){
+    document.querySelector('.burger').src=anything;
+}
+function changecolor(color){
+    const circle=document.querySelector('.circle');
+    circle.style.background=color;
+}
